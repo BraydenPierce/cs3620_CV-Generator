a394481716ecc40e7e0473d6297ebaa08df2ed5d
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-04-01 16:39:42 -0600

    feat: successfuly saving form to database

--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -3,4 +3,21 @@ from .models import Profile
 
 # Create your views here.
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '') # Second parameter is a default.
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        summary = request.POST.get('summary', '')
+        degree = request.POST.get('degree', '')
+        school = request.POST.get('school', '')
+        university = request.POST.get('university', '')
+        previous_work = request.POST.get('previous_work', '')
+        skills = request.POST.get('skills', '')
+
+        profile = Profile(name=name, email=email, phone=phone, 
+                        summary=summary, degree=degree, school=school, 
+                        university=university, previous_work=previous_work, 
+                        skills=skills)
+        profile.save()
+
     return render(request, 'pdf/accept.html')
